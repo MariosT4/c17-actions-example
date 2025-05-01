86e2c53e83c5967b84a5d21b9cc7bfa0eb15e301
MariosT4 <micmartim@gmail.com>
2025-05-01 10:31:25 +0100

    Ruin stuff

--- test_main.py
+++ test_main.py
@@ -6,4 +6,4 @@ from main import estimate_frog_count
 def test_estimate_frog_count_returns_int():
     """Checks that the function returns an integer."""
 
-    assert isinstance(estimate_frog_count(), int)
+    assert isinstance(estimate_frog_count(), str)
